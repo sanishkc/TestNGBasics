90a7dccf47437c62953f37c76b7d41d7629e3d6a
Sanish Chandran <sanish.k.c@gmail.com>
2025-12-26 06:32:28 +0200

    Update AddCalculatorTest with comments

--- src/test/java/com/testng/basics/AddCalculatorTest.java
+++ src/test/java/com/testng/basics/AddCalculatorTest.java
@@ -1,22 +1,28 @@
 package com.testng.basics;
 
+// Import TestNG annotations
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class AddCalculatorTest {
-	
-	AddCalculator addCalculator;
 
+    // Reference to AddCalculator class
+    AddCalculator addCalculator;
+
+    // Runs once before any test methods in this class
     @BeforeClass
     public void setup() {
-        
-    	addCalculator = new AddCalculator();
-    }
-	
-    @Test
-    public void addTest() {
-        int result = addCalculator.add(5, 10);
-        System.out.println("Result of addition: " + result);
+        // Initialize AddCalculator object
+        addCalculator = new AddCalculator();
     }
 
+    // Test method to verify addition functionality
+    @Test
+    public void addTest() {
+        // Perform addition
+        int result = addCalculator.add(5, 10);
+
+        // Print the result
+        System.out.println("Result of addition: " + result);
+    }
 }
